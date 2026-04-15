f26a92d4ffdb6e434cf87bfbbced22135e0a0a72
SivasangariMuthusamy <sivasankarim2908@gmail.com>
2026-04-15 15:13:35 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        ReportCard r1 = new ReportCard(1001, "Ram", 90, 90, 90);
+        r1.calcResult();
+        r1.printResult();
+    }
+}
