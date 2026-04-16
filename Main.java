c160f27a1a8157078618b71148399a1a97329979
SivasangariMuthusamy <sivasankarim2908@gmail.com>
2026-04-16 12:07:10 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,16 @@
-
 public class Main {
     public static void main(String[] args) {
-        DomesticBill user1 = new DomesticBill("Siva", 150);
+        
+        // Project 1: Report Card
+        System.out.println("--- STUDENT REPORT CARD ---");
+        ReportCard r1 = new ReportCard(1001, "Siva", 90, 95, 88);
+        r1.calcResult();
+        r1.printResult();
+        System.out.println(); 
+
+        // Project 2: EB Bill
+        System.out.println("--- EB BILLING SYSTEM ---");
+        DomesticBill user1 = new DomesticBill("Sivasangari", 150);
         user1.printFinalBill();
     }
 }
