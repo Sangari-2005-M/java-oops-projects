3fc51e0125dc05d9f5b4454fce75a6aaaaa04c8f
SivasangariMuthusamy <sivasankarim2908@gmail.com>
2026-06-18 14:47:43 +0530

    Create BankAccount.java

---
+++ BankAccount.java
@@ -0,0 +1,27 @@
+public abstract class BankAccount {
+    private String accountNumber;
+    protected double balance;
+
+    public BankAccount(String accountNumber, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: ₹" + amount + " | New Balance: ₹" + balance);
+        }
+    }
+
+    public abstract void withdraw(double amount);
+}
+
